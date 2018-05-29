676820ae96a9c420436016b45d3b0845e417be55
dickfala <dickfala@gmail.com>
2018-05-29 15:25:27 +0800

    add firebase frameworks, but can't initial.

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirebaseDemo
+//
+//  Created by Administrator on 2018/5/29.
+//  Copyright © 2018年 yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
